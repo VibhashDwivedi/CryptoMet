891f019ba49d65a619d97b5b560ad74143e92c4f
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-01-20 07:53:32 +0530

    Update cryptocurrency tracker with new data source and additional features

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
        <h6 className='text-white  fw-light fs-4 px-5'> Explore the world of cryptocurrencies with our comprehensive tracker app - <span className='fw-bold'>  CryptoMeter</span>. Get real-time updates on a vast array of digital currencies, from Bitcoin to altcoins, with detailed information on prices, market capitalization, trading volumes, available supply and rank. Stay ahead of the market, make informed decisions, and manage your cryptocurrency investments with ease using our user-friendly interface.
 </h6></div>
 <p>
-    <h6 className='text-white fw-light fs-4 px-5'>  CryptoMeter is a cryptocurrency tracker that displays real-time data on the top 100 cryptocurrencies by market capitalization. The data is updated every minute, and the cryptocurrency prices are sourced from the Binance exchange. The cryptocurrency prices are displayed in USD.</h6>
+    <h6 className='text-white fw-light fs-4 px-5'>  CryptoMeter is a cryptocurrency tracker that displays real-time data on the top cryptocurrencies by market capitalization. The data is updated every minute, and the cryptocurrency data is sourced from coinstats API. The cryptocurrency prices are displayed in various currencies such as USD, INR, EUR etc. It also displays top CryptoNews to keep you updated about market.</h6>
 </p>
         </div>
        
